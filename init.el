dc24436c557cf613b02c0f16ce1bf5d9e27a59b0
Catherine David <miscatulated@gmail.com>
2020-12-12 13:25:15 -0800

    Theme

---
+++ init.el
@@ -0,0 +1,11 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/moe-theme.el/")
+(require 'moe-theme)
+
+(moe-dark)
